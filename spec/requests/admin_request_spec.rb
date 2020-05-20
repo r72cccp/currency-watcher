2bfe2a1ab3da5cbd2630fa82804a39ec4a7d186b
Sergeij Belevskij <bbelevskij@gmail.com>
2020-05-20 13:49:25 +0500

    Fix regional time settings in spec

--- spec/requests/admin_request_spec.rb
+++ spec/requests/admin_request_spec.rb
@@ -14,16 +14,16 @@ RSpec.describe 'Admins', type: :request do
 
   describe 'POST /admin' do
     before do
-      set_forced_currency_rate(pair: 'USD/RUB', buy: 70.00, sell: 74.00, expired_at: '2020-05-20 11:13:14')
+      set_forced_currency_rate(pair: 'USD/RUB', buy: 70.00, sell: 74.00, expired_at: '2020-05-20 11:13:14 +0500')
     end
 
     it 'returns http success' do
-      post '/admin', params: { pair: 'USD/RUB', buy: 71.11, sell: 73.33, expiredAt: '2020-05-20 11:12:13' }
+      post '/admin', params: { pair: 'USD/RUB', buy: 71.11, sell: 73.33, expiredAt: '2020-05-20 11:12:13 +0500' }
       expect(response).to have_http_status(:success)
     end
 
     it 'forces existed currency rates with bigger expired date' do
-      post '/admin', params: { pair: 'USD/RUB', buy: 71.11, sell: 73.33, expiredAt: '2020-05-20 11:12:13' }
+      post '/admin', params: { pair: 'USD/RUB', buy: 71.11, sell: 73.33, expiredAt: '2020-05-20 11:12:13 +0500' }
       currency_rate = CurrencyRate.current.slice(:buy, :pair, :sell, :ticker)
       expect(currency_rate[:ticker]).to eq 1589955133
     end
