2e91f73199503fce21bfd69266a3c856744d8445
Sergeij Belevskij <bbelevskij@gmail.com>
2020-05-25 17:03:02 +0500

    Deploy to heroku #10

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   config.hosts << 'dev.fun-box.ru'
   config.hosts << 'ancient-temple-64629.herokuapp.com'
@@ -22,7 +24,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
