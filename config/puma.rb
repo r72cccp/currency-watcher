23011b4a68c52e9f1677f0894dda7516a5b77858
Sergeij Belevskij <bbelevskij@gmail.com>
2020-05-25 16:51:43 +0500

    Deploy to heroku #8

--- config/puma.rb
+++ config/puma.rb
@@ -15,9 +15,7 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-puts "ENV['PORT']: #{ENV['$PORT']}"
-puts ENV
-port ENV['PORT']
+# port ENV['PORT']
 
 # Specifies the `environment` that Puma will run in.
 #
