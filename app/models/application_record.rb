c90651271c7ad925190e86a77b402560fa725f53
Sergeij Belevskij <bbelevskij@gmail.com>
2020-05-31 23:37:23 +0500

    Implement row_count_limit for Heroku hobby-dev plan limit.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  class << self
+    def create!(params)
+      if ENV['ROW_LIMIT']
+        existed_count = count
+        row_limit = ENV['ROW_LIMIT'].to_i
+
+        order(:created_at).limit(existed_count - row_limit).delete_all if existed_count > row_limit
+      end
+      super(params)
+    end
+  end
 end
--- config/initializers/arask.rb
+++ config/initializers/arask.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Arask.setup(true) do |arask|
-  # arask.create task: 'currency_monitoring:fetch_currency_rate', interval: 30.seconds
   arask.create script: 'CurrencyRateService.fetch_currency_rate', interval: 10.seconds
   arask.create script: 'CurrencyRateService.broadcast', interval: 10.seconds
-  # arask.on_exception do |exception, arask_job|
-  #   raise(exception)
-  # end
 end
